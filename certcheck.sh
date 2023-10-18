54c29f618a9b79eef3af85aec483708145ffbe9e
Sergei Valuev <s.valuev@propellerads.net>
2023-10-18 14:42:50 +0400

    Add .gitingnore. And some functional.

--- certcheck.sh
+++ certcheck.sh
@@ -1,43 +1,54 @@
 #!/usr/bin/env bash
 ###################
-#HELP             #
+# HELP            #
 ###################
+
+args=("$@")
 
 function license {
     echo "This script wtiten by Sergei \"feeler\" Valuev under GPL 3.0 license."
 }
 
 function full_help {
+    echo "ERROR: You need to pass a sub-command (e.g., certcheck.sh SUB-COMMAND)"
     test1_help; test2_help
+    echo -e "\nUse \"license\" to see script's license"
 }
 
-function test1_help {
-    echo -e "\nTest1 help to show you"
+function local_help {
+    echo -e "\n*** Local check ***\n\
+    ./certcheck.sh local [FILE] - use path to directory containing certificates.
+    Script will search recursevly. E.g., ./certcheck.sh local /ect/nginx/ssl"
 }
 
 function test2_help {
     echo -e "\nTest2 help to show you"
 }
 
+function echo_params {
+    echo "$args"
+}
+
 ###################
-#MAIN SCRIPT      #
+# MAIN SCRIPT     #
 ###################
 
 main() {
 case "$1" in
-    "help" | ""     ) full_help;;
-    "license"       ) license;;
-    "test1"          ) test1;;
-    "test2"          ) test2;;
-    *) inv_param;;
+    "help" | ""      ) full_help && exit 1;;
+    "license"        ) license;;
+    "local"          ) local "$@";;
+    "test2"          ) test2 "$@";;
+    *) invalid_param && exit 1;;
 esac
 }
 
-function test1 {
+function local {
 shift
 case "$1" in
-    "help" | ""     ) test1_help;;
-    "case1"         ) echo "case2";;
+    "help" | ""     ) local_help && exit 1;;
+    "case1"         ) echo "case1";;   ### revome it after finish of the block of code
+    *) local_search;;
 esac
 }
 
@@ -46,18 +57,31 @@ shift
 case "$1" in
     "help" | ""     ) test2_help;;
     "case2"         ) echo "case2";;
+    *) local_search "$@";;
 esac
 }
 
 
-#debug this shit
-function inv_param {
+###################
+# FUNCTIONS       #
+###################
+
+
+function invalid_param {
     echo -e "Invalid parameter.\nUse \"help\" as a script parameter to know how to use it."
 }
 
 
 main "$@"; exit
 
+function local_search {
+    for var in $(find $1 -type f -name "*.crt")
+    do
+    #cat echo $var
+    openssl x509 -in $var -noout -dates -dateopt iso_8601 -subject | grep -v notBefore | tr '\n' '\tab'; echo -e '\n' | sort -dr
+    #paste $exp_date $cert_path
+    done
+}
 
 
 
