caca5da757278d2332702d23b4ce16deb0c414c6
Sergei Valuev <s.valuev@propellerads.net>
2023-10-22 13:00:08 +0400

    add a draft

--- drafts.sh
+++ drafts.sh
@@ -6,3 +6,18 @@ openssl x509  -noout -dates -dateopt iso_8601 -subject -in "$var" | grep -v notB
 #paste $exp_date $cert_path
 done
 ##################
+
+
+function local_search {
+    shift
+#    echo "local_search func: $*, number is $#, first is $1"
+    find "$1" -name '*.crt*' -exec sh -c '
+        openssl x509  -noout -dates -dateopt iso_8601 -subject -in {} |\
+        tr "\n" "\t" | sort -dr \;'
+#    for var in $(find $1 -type f -name "*.crt")
+#    do
+#        openssl x509  -noout -dates -dateopt iso_8601 -subject -in "$var" |\
+#        tr '\n' '\t'; echo -e "$var" |\
+#        awk 'BEGIN {FS="\t"; OFS="\t"} {print $2 $1 $4 $3}' |\
+#        sort -dr
+#    done
