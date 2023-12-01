0a96696c035bbc5762dbf581ed75e22190caeb42
Sergei Valuev <s.valuev@propellerads.net>
2023-12-01 14:48:41 +0400

    Add drafts.sh

--- drafts.sh
+++ drafts.sh
@@ -1,12 +1,9 @@
-##################
 for var in $(find $1 -type f -name "*.crt")
 do
 #cat echo $var
 openssl x509  -noout -dates -dateopt iso_8601 -subject -in "$var" | grep -v notBefore | tr '\n' '\tab'; echo -e "$var" | sort -dr
 #paste $exp_date $cert_path
 done
-##################
-
 
 function local_search {
     shift
@@ -21,16 +18,34 @@ function local_search {
 #        awk 'BEGIN {FS="\t"; OFS="\t"} {print $2 $1 $4 $3}' |\
 #        sort -dr
 #    done
-
-
-
-function domentor_check7 {
-    curl -s GET "https://api.domentor.rtty.in/certs?expire_period=7" |\
-    jq -r '.items[] | "\(.type) \(.days_to_expire) \(.name)"' |\
-    #TYPE_POSITION var comes from here ^^^
-    awk '{ printf "%-5s %-25s %-15s\n", $1, $2, $3 }' |\
-    sed 's/8/Technical/g; s/11/Zeydoo/g; s/6/NativeAds/g' |\
-    sort -k1,1nr
 }
-
-
+-------------------------------------
+function readarray {
+    read -at A_records <<< "$(aws route53 list-resource-record-sets \
+    --hosted-zone-id "$zone_id" \
+    --output json | \
+    jq -c '.ResourceRecordSets[] | select(.Type == "A") | "\(.ResourceRecords[0].Value) \(.Name)"' |\
+    tr -d '"' | \
+    awk '{print $2}' | tr '\n' ' ')"
+        
+for record in "${A_records[@]}"
+do
+    echo "$record"
+done
+}
+-------------------------------------
+openssl rsa -noout -modulus -in /path/to/your/private.key  2> /dev/null | openssl md5
+openssl x509 -noout -modulus -in /path/to/your/certificate.crt | openssl md5
+---------------------------------------
+function tls_1 {
+echo "${FUNCNAME[1]}"
+server_name=$1
+port=443
+#    echo "$server_name"; echo "$port"
+    echo -n unlock | openssl s_client -servername "$server_name" -connect "$server_name":"$port" 2>&1 |\
+    openssl x509 -noout -dates -dateopt iso_8601 -subject |\
+    grep -E 'subject|notBefore|notAfter' |\
+    tr "\n" "\t"; echo -e "$1" |\
+    sort -dr | awk -F'\t' '{print $2 "\t" $1 "\t" $4 "\t" $3}'
+}
+-------------------------------------
